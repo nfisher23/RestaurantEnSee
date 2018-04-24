168b484f1cc1a255cb2191fb2b40efd7c1b591e3
Nick Fisher <fisher.nick14@gmail.com>
2018-04-24 15:12:16 -0700

    Models created

---
+++ RestaurantEnSee/Areas/Home/Models/FoodCategory.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RestaurantEnSee.Areas.Home.Models
+{
+    public class FoodCategory
+    {
+        public int FoodCategoryId { get; set; }
+        public string Title { get; set; }
+        public IEnumerable<MenuItem> FoodItems { get; set; }
+    }
+}
---
+++ RestaurantEnSee/Areas/Home/Models/Menu.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RestaurantEnSee.Areas.Home.Models
+{
+    public class Menu
+    {
+        public int MenuId { get; set; }
+        public IEnumerable<FoodCategory> Categories { get; set; }
+    }
+}
